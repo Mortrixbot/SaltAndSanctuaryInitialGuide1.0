95c4abd6782e33ecb39305699674ac50828941e4
Mortrixbot <br3n0d4rv1n@gmail.com>
2022-03-23 18:10:46 -0300

    NLW

--- main.js
+++ main.js
@@ -24,9 +24,9 @@ for (const link of links) {
 /*-------------------------------------------------------------------------------------------------------------------------------*/
 /* = = = | Mudar o Header Da Página Ao Scroll | = = = */
 
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
 function changeHeaderWhenScroll() {
-  const header = document.querySelector('#header')
-  const navHeight = header.offsetHeight
   if (window.scrollY >= navHeight) {
     // Scroll é Maior Que a Altura Do Header //
     header.classList.add('scroll')
@@ -46,7 +46,13 @@ const swiper = new Swiper('.swiper-container', {
     el: '.swiper-pagination'
   },
   mousewheel: true,
-  keyboard: true
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
 })
 
 /* = = = = = = = = = = = = = = = = = = = = = = = = = */
@@ -73,19 +79,47 @@ scrollReveal.reveal(
 /*-------------------------------------------------------------------------------------------------------------------------------*/
 /* = =  = = = = = = = = | Botão "Back to top" | = = = = = = = = = = */
 
+const backToTopButton = document.querySelector('.back-to-top')
+
 function backToTop() {
-  const backToTopButton = document.querySelector('.back-to-top')
   if (window.scrollY >= 560) {
     backToTopButton.classList.add('show')
   } else {
     backToTopButton.classList.remove('show')
   }
 }
+
+/* = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = */
+/*-------------------------------------------------------------------------------------------------------------------------------*/
+/* = =  = = = = = = = = | MMenu Ativo Conforme a Seção Visível Na Página | = = = = = = = = = = */
+
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window, innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
 // When Scroll //
 window.addEventListener('scroll', function () {
   changeHeaderWhenScroll()
   backToTop()
+  activateMenuAtCurrentSection()
 })
-
-/* = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = */
-/*-------------------------------------------------------------------------------------------------------------------------------*/
